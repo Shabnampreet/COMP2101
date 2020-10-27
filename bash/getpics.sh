07c5043c17ea2660aa0502bcbfe5b19f1fea6964
Shabnampreet <2000shabnampreet@gmail.com>
2020-10-26 21:17:50 -0400

    get

--- bash/getpics.sh
+++ bash/getpics.sh
@@ -1,33 +1,11 @@
-#!/bin/bash
-#
-# this script gets some picture files for our personal web pages, which are kept in public_html
-# the pictures are put into a subdirectory of public_html named pics
-# it does some error checking
-# it summarizes the public_html/pics directory when it is done
-#
-# It should not be run as root
-[ "`whoami`" = "root" ] && echo "Not to be run as root" && exit 1
+--2020-10-26 21:12:25--  https://zonzorp.github.io/COMP2101/Labs/bash/scripts-lab2/getpics.sh
+Resolving zonzorp.github.io (zonzorp.github.io)... 185.199.109.153, 185.199.110.153, 185.199.111.153, ...
+Connecting to zonzorp.github.io (zonzorp.github.io)|185.199.109.153|:443... connected.
+HTTP request sent, awaiting response... 200 OK
+Length: 1926 (1.9K) [application/x-sh]
+Saving to: ‘getpics.sh’
 
-# Task1: Improve this script to also retrieve and install the files kept in the https://zonzorp.net/pics.tgz tarfile
-#   -use the same kind of testing that is already in the script to only download the tarfile if you don't already have it and
-#    to make sure the download and extraction commands work
-#    then delete the local copy of the tarfile if the extraction was successful
+     0K .                                                     100% 6.08M=0s
 
-# make sure we have a clean pics directory to start with
-rm -rf ~/public_html/pics
-mkdir -p ~/public_html/pics // (echo "Failed to make a new pics directory" && exit 1)
+2020-10-26 21:12:25 (6.08 MB/s) - ‘getpics.sh’ saved [1926/1926]
 
-# downlaod a zipfile of pictures to our Pictures directory - assumes you are online
-# unpack the download zipfile if the download succeeded - assumes we have an unzip command on this system
-# delete the local copy of the zipfile after a successful unpack of the zipfile
-wget -q -o ~/public_html/pics/pics.zip https://zonzorp.net/pics.zip && unzip -d ~/public_html/pics -o -q ~/public_html/pics/pics.zip && rm ~/public_html/pics/pics.zip
-
-# Task 1: Improve this script to also retrieve and install the files kept in the https://zonzorp.net/pics.tgz tarfile
-#     test to make sure the download and extraction commands work
-#     then delete the local copy of the tarfile if the extraction was successful
-
-# Make a report on what we have in the Pictures directory
-test -d ~/public_html/pics && cat <<EOF
-Found $(find ~/public_html/pics -type f/wc -1) files in the public_html/pics directory.
-The public_html/pics directory uses $(du -sh ~/public_html/pics /awk '{print $1}') space on the disk.
-EOF
